385a11335288c250d884bbb3c677f4daec8a5434
youngjinc <cyjin6789@gmail.com>
2023-06-19 18:16:50 +0900

    Test rebase

--- app/src/main/java/org/sopt/test/MainActivity.kt
+++ app/src/main/java/org/sopt/test/MainActivity.kt
@@ -1,14 +1,11 @@
 package org.sopt.test
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Toast.makeText(this, "Hello World", Toast.LENGTH_LONG)
     }
 }
