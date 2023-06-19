58021102df81422f417162208e9b2eb25ae92819
youngjinc <cyjin6789@gmail.com>
2023-06-19 17:24:30 +0900

    Delete toast

--- app/src/main/java/org/sopt/test/MainActivity.kt
+++ app/src/main/java/org/sopt/test/MainActivity.kt
@@ -1,17 +1,11 @@
 package org.sopt.test
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Toast.makeText(this, "Hello World1", Toast.LENGTH_SHORT).show()
-        Toast.makeText(this, "Hello World2", Toast.LENGTH_SHORT).show()
-        Toast.makeText(this, "Hello World3", Toast.LENGTH_LONG).show()
-        Toast.makeText(this, "Hello World4", Toast.LENGTH_LONG).show()
     }
 }
