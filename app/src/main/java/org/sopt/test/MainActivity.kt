936a43a681ef570e0eac2b32ebb4e05dd0bb4f6b
youngjinc <cyjin6789@gmail.com>
2023-06-19 17:58:26 +0900

    Add toast

--- app/src/main/java/org/sopt/test/MainActivity.kt
+++ app/src/main/java/org/sopt/test/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Toast.makeText(this, "Hello", Toast.LENGTH_LONG)
+        Toast.makeText(this, "Hello1", Toast.LENGTH_LONG)
+        Toast.makeText(this, "Hello2", Toast.LENGTH_LONG)
     }
 }
