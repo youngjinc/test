5e4018a04a092aab8df79e266cddb493de921efa
youngjinc <cyjin6789@gmail.com>
2023-06-19 16:28:09 +0900

    Add toast

--- app/src/main/java/org/sopt/test/MainActivity.kt
+++ app/src/main/java/org/sopt/test/MainActivity.kt
@@ -1,11 +1,14 @@
 package org.sopt.test
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show()
     }
 }
