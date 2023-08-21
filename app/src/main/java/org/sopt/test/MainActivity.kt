64366a6269fcdebc9fd46e0f372b8ba6c6581c10
choiyoungjin <cyjin6789@gmail.com>
2023-08-21 21:15:25 +0900

    Test rebase3 #2

--- app/src/main/java/org/sopt/test/MainActivity.kt
+++ app/src/main/java/org/sopt/test/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d(this::class.java.simpleName, "Hello2")
+        Log.d(this::class.java.simpleName, "Hello")
     }
 }
